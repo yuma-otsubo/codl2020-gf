8bbf03e2cc1fbd80cb20a31830cba2256f3da08b
tanitai <taniguchi.taisuke.18@shizuoka.ac.jp>
2020-12-01 15:31:17 +0900

    revised

--- client.py
+++ client.py
@@ -4,26 +4,26 @@
 import sys
 from socket import *
 import pbl2
-server_port = int(sys.argv[1])
-server_name = sys.argv[2]
-client_socket = socket(AF_INET,SOCK_STREAM)
-client_socket.connect((server_name,server_port))
+# server_port = int(sys.argv[1])
+# server_name = sys.argv[2]
+# client_socket = socket(AF_INET,SOCK_STREAM)
+# client_socket.connect((server_name,server_port))
 
-sentence = sys.argv[3]
+# sentence = sys.argv[3]
 
-client_socket.send(sentence.encode())
-if sentence == "SIZE":
-    received_ans = "size"
-elif sentence == "GET":
-    received_ans = "GET"
-elif sentence == "REP":
-    received_ans = "REP"
+# client_socket.send(sentence.encode())
+# if sentence == "SIZE":
+#     received_ans = "size"
+# elif sentence == "GET":
+#     received_ans = "GET"
+# elif sentence == "REP":
+#     received_ans = "REP"
 
-ans = client_socket.recv(1024)
-print(ans.decode())
+# ans = client_socket.recv(1024)
+# print(ans.decode())
 
-def filesize(s):
-    pass
+# def filesize(s):
+#     pass
 
 if if __name__ == "__main__":
     server_port = int(sys.argv[1])
