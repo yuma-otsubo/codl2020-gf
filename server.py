cf71336433b699dc682686265b6943eab6f791d7
yuma-otsubo <otsubo.yuma.18@shizuoka.ac.jp>
2020-12-02 19:05:28 +0900

    ここからここまでわかんない

--- server.py
+++ server.py
@@ -2,22 +2,47 @@
 
 import sys
 import time
+import os
 from socket import *
 BUFSIZE =5300 #一度に受信するデータサイズ
-file_name =sys.arv[1]
+file_name ='random_1M.dat'#読み書きするファイルサイズ
 
+##
+#ファイル分割するやつ
+def divide_file(filePath,chunkSize):
+    readedDAtaSize=0
+    i=0
+    fileList=[]
+    f=open(filePath,"rb")
+    contentLength=os.path.getsize(filePath)#読み終わるまで繰り返す
+    while readedDataSize<contentLEngth:
+          f.seek(readedDataSize)#読み取り位置をシーク
+          data=f.read(chunkSize)#指定したサイズだけ読み込む
+          saveFilePath=filePath+"."+str(i)#分割ファイル保存
+          with open(saveFilePath,'wb')as saveFile:
+              saveFile.write(data)
+          readedDataSize=readedDataSize+len(data)#読み込んだデータサイズの更新
+          i=i+1
+          fileList.append(saveFilePath)
+    return fileList
+
+##
 def interact_with_client(s):
     start_time =time.time()
     req_msg =s.recv(BUFSIZE).decode()#最大BUFSIZEバイトを受信する
     send_size=int(req_msg.split()[0])#最初の空白文字までに書かれているのがサイズなのでそれを解釈する
     with open(file_name,'rb') as f:
-	 data=f.read(send_size)
-	 s.send(data)
+         data=f.read(send_size)
+         s.send(data)
     end_time=time.time()
     elapsed_time=end_time -start_time
     try:#GET
-       #1で処理
-       code="OK Sending" +file_name + "from" + x + "to" + y +"total"+ (y-x) + "bytes at" + elapsed_time +"\n"
+       sentence=s.recv(1024).decode()
+       x=10 #GETのXXXX
+       y=int(sentence)#GETのYYYY
+       z=y-x+
+        #1で処理
+       code="OK Sending" +name + "from" + x + "to" + y +"total"+ z + "bytes at" + elapsed_time +"\n"
        s.send(code.encode())
     except OSError:
         code="NG 101 No such file"
