b31c7bcb554e7c9d4e3b2e0aceee83c1884ed95e
yuma-otsubo <otsubo.yuma.18@shizuoka.ac.jp>
2020-12-07 17:00:05 +0900

    aaa

--- server.py
+++ server.py
@@ -2,6 +2,7 @@
 
 import sys
 import time
+import os
 from socket import *
 BUFSIZE =5300 #一度に受信するデータサイズ
 file_name =sys.arv[1]
@@ -17,20 +18,49 @@ def interact_with_client(s):
     elapsed_time=end_time -start_time
     try:#GET
        #1で処理
-       code="OK Sending" +file_name + "from" + x + "to" + y +"total"+ (y-x) + "bytes at" + elapsed_time +"\n"
+       words[0]='GET'
+       file_name=words[1]
+       x=float(word[2])#pbl2.genkey()の結果を予め送る方法がわかんない
+       y=float(word[3])
+       code="OK Sending" +file_name + "from" + x + "to" + y +"total"+ (y-x+1) + "bytes at" + elapsed_time +"\n"
        s.send(code.encode())
-    except OSError:
+    except FileNotFoundError:
         code="NG 101 No such file"
         s.send(code.encode())
 	#print("NG 101 No such file")
-    except NameError:
+    except SyntaxError:#ここ自信ない
         code="NG 301 Invalid command"
         s.send(code.encode())
 	#print("NG 301 Invalid command")
-    except AttributeError:
+    except KeyError:
         code="NG 102 Invalid range"
         s.send(code.encode())
 	#print("NG 103 Invalid command")
+
+    try:#SIZE
+       words[0]='SIZE'
+       words[1]=file_name
+       code="OK "+file_name+" "+os.path.getsize()+" bytes"
+    except FileNotFoundError:
+        code="NG 101 No such file"
+        s.send(code.encode())
+    except SyntaxError#ここも同様
+        code="NG 301 Invalid command"
+        s.send(code.encode())
+    
+    try:#REP
+       words[0]='REP'
+       words[1]=file_name
+       code="OK digest of "+file_name +" was successfully received REP at " +end_time
+    except FileNotFoundError:
+        code="NG 101 no such file"
+        s.send(code.encode())
+    except SyntaxError:
+        code="NG 301 Invalid command"
+        s.send(code.encode())
+    except Error:
+    #時間の都合で途中
+    
     s.close()
 
 a_number=44
